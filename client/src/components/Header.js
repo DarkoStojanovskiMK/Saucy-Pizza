48566dddf63b378e0f3dcf0a927a8e0c259c2e84
Darko Stojanovski <darko.stojanovski.mk@gmail.com>
2022-01-03 19:13:21 +0100

    deploy4

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
                                     <LinkContainer to='/profile'>
                                         <NavDropdown.Item> Profile</NavDropdown.Item>
                                     </LinkContainer>
-                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                    <NavDropdown.Item onClick={logoutHandler}>Log Out</NavDropdown.Item>
                                 </NavDropdown>
                                 ) : (
                                 <LinkContainer to='/login'>
