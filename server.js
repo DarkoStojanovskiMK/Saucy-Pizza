d38ba05a38e933d0b3cbb06cab2663a48b6be318
Darko Stojanovski <darko.stojanovski.mk@gmail.com>
2022-01-03 18:39:22 +0100

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from'dotenv'
 import connectDB from './config/db.js'
 import pizzaRoutes from './routes/pizzaRoutes.js'
@@ -13,15 +14,26 @@ const app = express()
 
 
 
-app.get('/', (req,res)=>{
-    res.send('API is running...')
-})
 
-app.use(express.json({extended:false}))
+app.use(express.json())
 
 app.use('/api/pizzas', pizzaRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+const __dirname = path.resolve()
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    
+    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}else{
+    app.get('/', (req,res)=>{
+    res.send('API is running')
+})
+}
+
 
 
 
